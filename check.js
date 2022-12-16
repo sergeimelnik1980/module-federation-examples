1f72fc6b235862f4d6be1c52e1f58f10d64c116d
Sergey Melnik <sergey.melnik@valor-software.com>
2022-12-16 18:23:13 +0100

    feat(e2e:ci) add pipline34

--- check.js
+++ check.js
@@ -29,8 +29,21 @@
 // });
 
 ////////////////////////////////////////////////////////////////////////////////
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 
+// exec("npx lerna changed --json", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     try {
+//       const jsonArray = JSON.parse(stdout);
+//     console.log(JSON.stringify( {include: jsonArray.map(x => x.name)}, null, 2))
+//     } catch (e) {
+//       console.error(e)
+//     }
+// });
+const { exec } = require("child_process");
 exec("npx lerna changed --json", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
@@ -38,7 +51,7 @@ exec("npx lerna changed --json", (error, stdout, stderr) => {
     }
     try {
       const jsonArray = JSON.parse(stdout);
-      console.log(JSON.stringify({ package: jsonArray.map(x => x.name)}, null, 2))
+      console.log(jsonArray.map(x => '"'+x.name+'"').join(","));
     } catch (e) {
       console.error(e)
     }
